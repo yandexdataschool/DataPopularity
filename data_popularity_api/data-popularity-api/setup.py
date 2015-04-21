5b146cbd43dc6d923a4ca414e9c17981d166fd3f
hushchyn-mikhail <mikhail91@yandex-team.ru>
2015-04-21 12:07:41 +0300

    data-popularity-api in separate folder.

---
+++ data_popularity_api/data-popularity-api/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data-popularity-api',
+    version='0.1',
+    url='',
+    license='',
+    packages=find_packages(),
+    author='mikhail91',
+    author_email='mikhail91@yandex-team.ru',
+    description='Api service for DataPopularity module',
+    install_requires=[
+        'rep==0.5.0',
+        'DataPopularity==0.2'
+    ],
+)
--- data_popularity_api/debpackage/usr/bin/setup.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-sudo easy_install pip
-sudo apt-get install python-numpy
-sudo pip install numpy==1.9.1
