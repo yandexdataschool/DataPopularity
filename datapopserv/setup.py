4d77fc41e2cd712eeae53d148618fba92f3a258d
hushchyn-mikhail <mikhail91@yandex-team.ru>
2015-04-23 20:30:51 +0300

    setup fixed.

--- datapopserv/setup.py
+++ datapopserv/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datapopserv',
-    version='0.1',
+    version='0.1.0',
     url='https://github.com/hushchyn-mikhail/DataPopularity',
     license='',
     packages=find_packages(),
@@ -14,6 +14,6 @@ setup(
         'flask-restful == 0.3.2',
         'Werkzeug == 0.10.1',
         'rep==0.5.0',
-        'datapop==0.2'
+        'datapop==0.2.0'
     ],
 )
