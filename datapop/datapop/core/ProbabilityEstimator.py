16b5c043898158cd753f24dbcdac1d0bcb6d5fdf
Mihail Guschin <mikhail91@213.180.216.238-red.dhcp.yndx.net>
2015-07-02 18:18:22 +0300

    ProbabilityEstimator class was upgraded.

--- datapop/datapop/core/ProbabilityEstimator.py
+++ datapop/datapop/core/ProbabilityEstimator.py
@@ -20,15 +20,15 @@ from sklearn.ensemble import GradientBoostingClassifier
 
 class ProbabilityEstimator(object):
     """
-    This class computes datasets probabilities to be stored for given type of data storages.
+    This class predicts future dataset probabilities to be stored on given types of data storages.
     Parameters:
     -----------
-    :param pandas.DataFrame data: data to compute the probabilities. The data should contain following columns:
-    'ID' - unique name for each dataset; '1', '2', '3', ... - columns for datasets usage history time series.
+    :param pandas.DataFrame data: data to predict the probabilities. The data should contain following columns:
+    'ID' - unique name for each dataset; '1', '2', '3', ... - columns for dataset usage history time series.
 
-    :param int forecast_horizont: number of lastr time periods of datasets time series for which the probabilities will compute.
+    :param int forecast_horizont: number of last time periods of dataset time series for which the probabilities will predict.
 
-    :param int or list[int] class_abs_thresholds: datasets usage threshold values which is used to estimate data storage type for each dataset.
+    :param int or list[int] class_abs_thresholds: dataset usage threshold values which is used to estimate data storage type for each dataset.
     For example, class_abs_thresholds=[a,b,c] means that all datasets which have number of usages for last forecast_horizont time periods in range [0, a)
      is stored on type 1 storage; [a, b) - type 2 storage; [b, inf) - type 3 storage.
     """
@@ -50,11 +50,11 @@ class ProbabilityEstimator(object):
         Parameters:
         -----------
         :param pandas.DataFrame data: data to compute the probabilities. The data should contain following columns:
-        'ID' - unique name for each dataset; '1', '2', '3', ... - columns for datasets usage history time series.
+        'ID' - unique name for each dataset; '1', '2', '3', ... - columns for dataset usage history time series.
 
-        :param int forecast_horizont: number of lastr time periods of datasets time series for which the probabilities will compute.
+        :param int forecast_horizont: number of last time periods of dataset time series for which the probabilities will compute.
 
-        :param int or list[int] class_abs_thresholds: datasets usage threshold values which is used to estimate data storage type for each dataset.
+        :param int or list[int] class_abs_thresholds: dataset usage threshold values which is used to estimate data storage type for each dataset.
         For example, class_abs_thresholds=[a,b,c] means that all datasets which have number of usages for last forecast_horizont time periods in range [0, a)
          is stored on type 1 storage; [a, b) - type 2 storage; [b, inf) - type 3 storage.
         """
@@ -75,16 +75,16 @@ class ProbabilityEstimator(object):
         Parameters:
         -----------
         :param pandas.DataFrame data: data to compute the probabilities. The data should contain following columns:
-        'ID' - unique name for each dataset; '1', '2', '3', ... - columns for datasets usage history time series.
+        'ID' - unique name for each dataset; '1', '2', '3', ... - columns for dataset usage history time series.
 
-        :param int forecast_horizont: number of lastr time periods of datasets time series for which the probabilities will compute.
+        :param int forecast_horizont: number of last time periods of dataset time series for which the probabilities will compute.
 
         :param int or list[int] class_abs_thresholds: datasets usage threshold values which is used to estimate data storage type for each dataset.
         For example, class_abs_thresholds=[a,b,c] means that all datasets which have number of usages for last forecast_horizont time periods in range [0, a)
          is stored on type 1 storage; [a, b) - type 2 storage; [b, inf) - type 3 storage.
 
         :return pandas.DataFrame preprocessed_data: the data prepared for classifier.
-        numpy.array number_columns: array of column names for the datasets time series.
+        numpy.array number_columns: array of column names for the dataset time series.
         """
         #Find time series columns
         columns = data.columns
@@ -97,74 +97,112 @@ class ProbabilityEstimator(object):
             type_label = type_label + (fh_data_values >= class_abs_thresholds[type_num])*(1) #Storage type type_num + 1 TODO
 
         train_num_cols = number_columns[:-forecast_horizont]
-        preprocessed_data = pd.DataFrame(columns=['ID']+train_num_cols+['FirstUsage', 'Type'])
-        preprocessed_data['ID'] = data['ID'].values
-        preprocessed_data[train_num_cols] = data[train_num_cols].values
-        preprocessed_data['FirstUsage'] = (data[number_columns].values.cumsum(axis=1)!=0).sum(axis=1)
-        preprocessed_data['Type'] = type_label
+        train_data = pd.DataFrame(columns=['ID']+train_num_cols+['FirstUsage', 'Type'])
+        train_data['ID'] = data['ID'].values
+        train_data[train_num_cols] = data[train_num_cols].values
+        train_data['FirstUsage'] = (data[number_columns].values.cumsum(axis=1)!=0).sum(axis=1)
+        train_data['Type'] = type_label
         train_cols = train_num_cols + ['FirstUsage']
-        return preprocessed_data, train_cols
 
-    def get_probabilities(self):
+        test_num_cols = number_columns[forecast_horizont:]
+        test_data = pd.DataFrame(columns=['ID']+test_num_cols+['FirstUsage'])
+        test_data['ID'] = data['ID'].values
+        test_data[test_num_cols] = data[test_num_cols].values
+        test_data['FirstUsage'] = (data[number_columns].values.cumsum(axis=1)!=0).sum(axis=1)
+        test_cols = test_num_cols + ['FirstUsage']
+
+        return train_data, train_cols, test_data, test_cols
+
+    def _get_current_probabilities(self):
         """
-        This method is used to compute and get probabilities to be stored on each data storage type for each dataset.
-        :return: pandas.DataFrame report: the raport contain following columns: 'ID' - unique name for each dataset;
+        This method is used to estimate current probabilities to be stored on each data storage type for each dataset.
+        :return: pandas.DataFrame report: the report contains following columns: 'ID' - unique name for each dataset;
         'Current_Type' - current data storage type for each dataset estimated on the last forecast_horizont time periods;
         'Proba_Type_i' - datasets probabilities to be stored on storage type i.
         """
-        preprocessed_data, train_columns = self._data_preprocessing(self.data, self.forecast_horizont, self.class_abs_thresholds)
-        self.preprocessed_data = preprocessed_data.copy() #Used just for output
-        X = preprocessed_data[train_columns].astype(np.float).values
-        Y = preprocessed_data['Type'].values
+        train_data, train_columns, _, _ = self._data_preprocessing(self.data, self.forecast_horizont, self.class_abs_thresholds)
+        X = train_data[train_columns].astype(np.float).values
+        Y = train_data['Type'].values
 
         try:
             n_folds = 3
             folder = FoldingClassifier(GradientBoostingClassifier(learning_rate=0.02, n_estimators=2500, max_depth=6, subsample=0.8),\
                                        n_folds=n_folds, features=None, random_state=42)
             folder.fit(X, Y)
-            self.classifier = folder #Used just for output
         except:
             print ("Can not train classifier. Please, check data.")
 
         probabilities = folder.predict_proba(X)
         classes = folder.classes_
 
-        report = pd.DataFrame()
-        report['ID'] = preprocessed_data['ID'].values
-        report['Current_Type'] = preprocessed_data['Type']
-        #In case of multiclasses
-        for i in range(0, len(classes)):
-            class_i = classes[i]
-            report['Proba_Type_%d' % class_i] = probabilities[:,i]
+        report_proba_cols = ['Proba_Type_%d' % classes[i] for i in range(0, len(classes))]
+        report = pd.DataFrame(columns=['ID', 'Current_Type']+report_proba_cols)
+        report['ID'] = train_data['ID'].values
+        report['Current_Type'] = train_data['Type']
+        report[report_proba_cols] = probabilities
         return report
 
 
     def _test_future_proba(self):
 
-        preprocessed_data_test, test_columns = self._data_preprocessing(self.data, self.forecast_horizont, self.class_abs_thresholds)
-        X_test = preprocessed_data_test[test_columns[self.forecast_horizont:]].astype(np.float).values
-        Y_test = preprocessed_data_test['Type'].values
+        test_data, test_columns, _, _ = self._data_preprocessing(self.data, self.forecast_horizont, self.class_abs_thresholds)
+        X_test = test_data[test_columns[self.forecast_horizont:]].astype(np.float).values
+        Y_test = test_data['Type'].values
 
-        preprocessed_data_train, train_columns = self._data_preprocessing(preprocessed_data_test[['ID']+test_columns], self.forecast_horizont, self.class_abs_thresholds)
-        X_train = preprocessed_data_train[train_columns].astype(np.float).values
-        Y_train = preprocessed_data_train['Type'].values
+        train_data, train_columns, _, _ = self._data_preprocessing(test_data[['ID']+test_columns], self.forecast_horizont, self.class_abs_thresholds)
+        X_train = train_data[train_columns].astype(np.float).values
+        Y_train = train_data['Type'].values
 
         n_folds = 2
         folder = FoldingClassifier(GradientBoostingClassifier(learning_rate=0.02, n_estimators=2500, max_depth=6, subsample=0.8),\
                                        n_folds=n_folds, features=None, random_state=42)
         folder.fit(X_train, Y_train)
 
-        probabilities_train = folder.predict_proba(X_train)
-        fpr_train, tpr_train, _ = roc_curve(Y_train, probabilities_train[:,1], pos_label=None, sample_weight=None)
+        train_probabilities = folder.predict_proba(X_train)
+        fpr_train, tpr_train, _ = roc_curve(Y_train, train_probabilities[:,1], pos_label=None, sample_weight=None)
         roc_auc_train = auc(fpr_train, tpr_train)
 
-        probabilities_test = folder.predict_proba(X_test)
-        fpr_test, tpr_test, _ = roc_curve(Y_test, probabilities_test[:,1], pos_label=None, sample_weight=None)
+        test_probabilities = folder.predict_proba(X_test)
+        fpr_test, tpr_test, _ = roc_curve(Y_test, test_probabilities[:,1], pos_label=None, sample_weight=None)
         roc_auc_test = auc(fpr_test, tpr_test)
 
-        return roc_auc_train, roc_auc_test
+        return roc_auc_train, roc_auc_test, train_probabilities, test_probabilities, Y_train, Y_test
 
+    def get_probabilities(self):
+        """
+        This method is used to predict probabilities to be stored on each data storage type for each dataset.
+        :return: pandas.DataFrame report: the report contains following columns: 'ID' - unique name for each dataset;
+        'Current_Type' - current data storage type for each dataset estimated on the last forecast_horizont time periods;
+        'Proba_Type_i' - datasets probabilities to be stored on storage type i.
+        """
 
+        train_data, train_columns, test_data, test_columns = self._data_preprocessing(self.data, self.forecast_horizont, self.class_abs_thresholds)
+        X_train = train_data[train_columns[self.forecast_horizont:]].astype(np.float).values
+        Y_train = train_data['Type'].values
+        X_test = test_data[test_columns].values
+
+        n_folds = 3
+        folder = FoldingClassifier(GradientBoostingClassifier(learning_rate=0.02, n_estimators=2500, max_depth=6, subsample=0.8),\
+                                       n_folds=n_folds, features=None, random_state=42)
+        folder.fit(X_train, Y_train)
+        train_probabilities = folder.predict_proba(X_train)
+        test_probabilities = folder.predict_proba(X_train)
+        classes = folder.classes_
+
+        train_report_proba_cols = ['Proba_Type_%d' % classes[i] for i in range(0, len(classes))]
+        train_report = pd.DataFrame(columns=['ID', 'Current_Type']+train_report_proba_cols)
+        train_report['ID'] = train_data['ID'].values
+        train_report['Current_Type'] = train_data['Type']
+        train_report[train_report_proba_cols] = train_probabilities
+        self.train_report = train_report
+
+        test_report_proba_cols = ['Proba_Type_%d' % classes[i] for i in range(0, len(classes))]
+        test_report = pd.DataFrame(columns=['ID']+test_report_proba_cols)
+        test_report['ID'] = test_data['ID'].values
+        test_report[test_report_proba_cols] = test_probabilities
+        self.test_report = test_report
+
+        return test_report
 
 
 
