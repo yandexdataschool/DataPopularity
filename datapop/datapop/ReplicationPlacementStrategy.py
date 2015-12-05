d360e23bc055748a024229a8807f382163a407c8
Mihail Guschin <mikhail91@mikhail91-osx.local>
2015-12-05 20:26:40 +0300

    kernel smoothing v.1

--- datapop/datapop/ReplicationPlacementStrategy.py
+++ datapop/datapop/ReplicationPlacementStrategy.py
@@ -121,19 +121,49 @@ class ReplicationPlacementStrategy(object):
         report['Name'] = to_predict_data['Name']
         report['Prediction'] = Y_pred_to_predict
         report['mpe'] = mpe
-        return report
+        return report, Y_pred_test, Y_test
+
+    def _max_reuse_distance(self, access_time_series):
+        max_reuse_distances_list = []
+        for dataset in access_time_series:
+            max_reuse_distance = 1
+            reuse_distance = 0
+            start_flag = 0
+            for access in dataset:
+                if access > 0:
+                    start_flag = 1
+                if start_flag == 1:
+                    reuse_distance += 1
+                if start_flag == 1 and access > 0:
+                    max_reuse_distance = max(max_reuse_distance, reuse_distance)
+                    reuse_distance = 1
+            max_reuse_distances_list.append(max_reuse_distance)
+        return numpy.array(max_reuse_distances_list)
+
+    def _total_fraquency_week(self, access_time_series):
+        return (1. * (access_time_series > 0)).sum(axis=1) + 1
 
     def _kernel_smoothing_predictor(self, access_time_series, first_used_list, window_to_past):
 
         kernel_predictions_list = []
-        for dataset, first_used in zip(access_time_series, first_used_list):
-            index = min(window_to_past, first_used)
+        max_reuse_distance_list = self._max_reuse_distance(access_time_series[:, -window_to_past:])
+        total_frequency_week_list = self._total_fraquency_week(access_time_series[:, -window_to_past:])
+        lists = zip(access_time_series, first_used_list, max_reuse_distance_list)
+        for dataset, first_used, max_reuse_distance in lists:
+            # index = min(window_to_past, first_used)
+            index = window_to_past
             x_coords = numpy.array([[x_val] for x_val in range(0, index)])
             y_values = dataset[-index:]
-            x_coord_pred = numpy.array([x_coords[-1] + 1])
-            # TODO: window width optimization
-            kernel_regressor = KernelRegression(kernel="rbf", gamma=1./169.)
-            kernel_prediction = kernel_regressor.fit(x_coords, y_values).predict(x_coord_pred)
+            #selection = total_frequency_week_list == total_frequency_week
+            #windows = max_reuse_distance_list[selection]
+            #window = int(numpy.percentile(windows, 80)) + 1
+            window = min(max_reuse_distance, index)
+            kernel_regressor = KernelRegression(kernel="rbf", gamma=numpy.logspace(-2, 2, 10))
+            y_kernel = kernel_regressor.fit(x_coords, y_values).predict(x_coords)
+            #y_kernel = y_values
+            #y_rolling_mean = pandas.rolling_mean(y_kernel, window=window, axis=1)
+            y_rolling_mean = y_kernel
+            kernel_prediction = y_rolling_mean[-1]
             kernel_predictions_list.append(kernel_prediction)
         return numpy.array(kernel_predictions_list)
 
@@ -167,4 +197,4 @@ class ReplicationPlacementStrategy(object):
         report['Name'] = to_predict_data['Name']
         report['Prediction'] = Y_pred_to_predict
         report['mpe'] = mpe
-        return report
+        return report, Y_pred_test, Y_test
